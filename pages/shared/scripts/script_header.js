0c665a9fd3276cd373c1b36fe1d964b760ad4209
Sean Bush <sbus139@wgu.edu>
2026-06-22 15:22:12 -0500

    Move header definiton to script and remove duplicate from all page indexes

--- pages/shared/scripts/script_header.js
+++ pages/shared/scripts/script_header.js
@@ -1,5 +1,36 @@
-const navMenuToggleButton = document.getElementById("nav-menu-toggle");
+const body = document.querySelector('body');
+
+const header_html = `
+        <header id="page-header">
+            <nav>
+                <a id="nav-home" href="/pages/homepage">Sean Bush</a>
+            </nav>
+            <nav id="nav-bar">
+                <a href="/pages/about">About</a>
+                <a href="/pages/blog">Blog</a>
+                <a href="/pages/portfolio">Portfolio</a>
+                <a href="/pages/resume">Resume</a>
+                <a href="/pages/contact">Contact</a>
+            </nav>
+            <div id="button-container">
+                <button id="nav-menu-toggle">≡</button>
+            </div>
+            <menu id="nav-menu" class="hide">
+                <li><a href="/pages/about">About</a></li>
+                <li><a href="/pages/blog">Blog</a></li>
+                <li><a href="/pages/portfolio">Portfolio</a></li>
+                <li><a href="/pages/resume">Resume</a></li>
+                <li><a href="/pages/contact">Contact</a></li>
+            </menu>
+        </header>
+
+        <div class="dummy-header"></div>
+`
+
+body.insertAdjacentHTML('afterbegin', header_html);
+
 const navMenu = document.getElementById("nav-menu");
+const navMenuToggleButton = document.getElementById("nav-menu-toggle");
 
 navMenuToggleButton.addEventListener("click", toggleMenu);
 function toggleMenu(event) {
